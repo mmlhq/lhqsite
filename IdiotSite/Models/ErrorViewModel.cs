14a32283423dc977738925d1f8ce088f570b2ebf
lihq <coolfreemail@sohu.com>
2018-04-06 21:17:13 +0800

    添加项目文件。

---
+++ IdiotSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdiotSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
